1b73fe644118c28ea0f772fb069bf34fe56fc7ea
e.vorontsov <e.vorontsov@eapteka.ru>
2024-11-27 15:27:39 +0300

    init

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,22 +1,53 @@
 import os
 import shutil
+import subprocess
+
+def clean_target_directory():
+    project_root = os.getcwd()
+    for item in os.listdir(project_root):
+        # Исключаем временные папки, чтобы случайно не удалить файлы шаблона
+        if item not in ["cookiecutter.json", "hooks"]:
+            item_path = os.path.join(project_root, item)
+            if os.path.isfile(item_path):
+                os.remove(item_path)
+            elif os.path.isdir(item_path):
+                shutil.rmtree(item_path)
+
+def copy_files(source_dir, destination_dir):
+    for item in os.listdir(source_dir):
+        src_path = os.path.join(source_dir, item)
+        dest_path = os.path.join(destination_dir, item)
+        if os.path.isdir(src_path):
+            shutil.copytree(src_path, dest_path)
+        else:
+            shutil.copy2(src_path, dest_path)
+
+def run_go_mod_tidy():
+    try:
+        subprocess.run(["go", "mod", "tidy"], check=True)
+    except FileNotFoundError:
+        print("Ошибка: Команда `go` не найдена. Убедитесь, что Go установлен.")
+    except subprocess.CalledProcessError as e:
+        print(f"Ошибка при выполнении `go mod tidy`: {e}")
 
 def setup_project():
     project_root = os.getcwd()
+    clean_target_directory()
 
+    # Определяем, какой шаблон использовать
     if "{{ cookiecutter.include_cobra }}" == "yes":
-        # Переместить содержимое with_cobra в корень
         source_dir = os.path.join(project_root, "cobra")
     else:
-        # Переместить содержимое without_cobra в корень
         source_dir = os.path.join(project_root, "simple")
 
-    # Переместить файлы из выбранного шаблона
-    for item in os.listdir(source_dir):
-        shutil.move(os.path.join(source_dir, item), project_root)
+    # Копируем содержимое выбранной папки
+    copy_files(source_dir, project_root)
 
-    # Удалить временные папки
+    # Удаляем временные папки шаблона
     shutil.rmtree(os.path.join(project_root, "cobra"), ignore_errors=True)
     shutil.rmtree(os.path.join(project_root, "simple"), ignore_errors=True)
+
+    # Выполняем go mod tidy
+    run_go_mod_tidy()
 
 setup_project()
