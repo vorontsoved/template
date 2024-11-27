e79fdc7190af05946e237d25b8424bad82fddddc
e.vorontsov <e.vorontsov@eapteka.ru>
2024-11-27 15:55:08 +0300

    init

--- {{cookiecutter.project_slug}}/hooks/post_gen_project.py
+++ {{cookiecutter.project_slug}}/hooks/post_gen_project.py
@@ -9,20 +9,9 @@ def setup_project():
     else:
         source_dir = os.path.join(project_root, "simple")
 
-    # Переместить только содержимое выбранного шаблона
+    # Переместить содержимое выбранной папки в корень
     for item in os.listdir(source_dir):
-        source_item = os.path.join(source_dir, item)
-        target_item = os.path.join(project_root, item)
-
-        if os.path.exists(target_item):
-            # Удалить существующий файл/папку
-            if os.path.isfile(target_item) or os.path.islink(target_item):
-                os.unlink(target_item)
-            elif os.path.isdir(target_item):
-                shutil.rmtree(target_item)
-
-        # Переместить содержимое в корень проекта
-        shutil.move(source_item, target_item)
+        shutil.move(os.path.join(source_dir, item), project_root)
 
     # Удалить временные папки
     shutil.rmtree(os.path.join(project_root, "cobra"), ignore_errors=True)
