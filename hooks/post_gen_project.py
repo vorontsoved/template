f47e59addc1e31fde14f2ba5912c0487d40b02a8
e.vorontsov <e.vorontsov@eapteka.ru>
2024-11-27 14:30:12 +0300

    init

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,10 +2,13 @@ import os
 import shutil
 import json
 
-def copy_template(project_type, project_slug):
-    """Копирует файлы из simple или cobra в итоговую директорию проекта"""
+def move_files(project_type, project_slug):
+    """Копирует файлы из папки simple или cobra в корень"""
     source_dir = os.path.join(project_type, "{{cookiecutter.project_slug}}")
     target_dir = "."
+
+    if not os.path.exists(source_dir):
+        raise ValueError(f"Не найдена папка-источник: {source_dir}")
 
     for item in os.listdir(source_dir):
         s = os.path.join(source_dir, item)
@@ -15,7 +18,7 @@ def copy_template(project_type, project_slug):
         else:
             shutil.copy2(s, d)
 
-    # Удаляем временные файлы после копирования
+    # Удаляем временные файлы
     shutil.rmtree(source_dir)
 
 def main():
@@ -29,7 +32,7 @@ def main():
     if project_type not in ["simple", "cobra"]:
         raise ValueError("Некорректный тип проекта. Выберите simple или cobra.")
 
-    copy_template(project_type, project_slug)
+    move_files(project_type, project_slug)
 
 if __name__ == "__main__":
     main()
