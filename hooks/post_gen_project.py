3b1663e99e37a205c4dab06e507910a679ca5f60
e.vorontsov <e.vorontsov@eapteka.ru>
2024-11-27 15:50:26 +0300

    init

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -13,13 +13,15 @@ def setup_project():
     for item in os.listdir(source_dir):
         source_item = os.path.join(source_dir, item)
         target_item = os.path.join(project_root, item)
+
         if os.path.exists(target_item):
-            # Если файл/директория уже существует, удаляем её
+            # Удалить существующий файл/папку
             if os.path.isfile(target_item) or os.path.islink(target_item):
                 os.unlink(target_item)
             elif os.path.isdir(target_item):
                 shutil.rmtree(target_item)
 
+        # Переместить содержимое в корень проекта
         shutil.move(source_item, target_item)
 
     # Удалить временные папки
