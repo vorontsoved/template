3f11a574a866bfe300e018f1db0ce77322c3f097
e.vorontsov <e.vorontsov@eapteka.ru>
2024-11-27 15:41:07 +0300

    init

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,15 +5,22 @@ def setup_project():
     project_root = os.getcwd()
 
     if "{{ cookiecutter.include_cobra }}" == "yes":
-        # Переместить содержимое with_cobra в корень
         source_dir = os.path.join(project_root, "cobra")
     else:
-        # Переместить содержимое without_cobra в корень
         source_dir = os.path.join(project_root, "simple")
 
-    # Переместить файлы из выбранного шаблона
+    # Переместить только содержимое выбранного шаблона
     for item in os.listdir(source_dir):
-        shutil.move(os.path.join(source_dir, item), project_root)
+        source_item = os.path.join(source_dir, item)
+        target_item = os.path.join(project_root, item)
+        if os.path.exists(target_item):
+            # Если файл/директория уже существует, удаляем её
+            if os.path.isfile(target_item) or os.path.islink(target_item):
+                os.unlink(target_item)
+            elif os.path.isdir(target_item):
+                shutil.rmtree(target_item)
+
+        shutil.move(source_item, target_item)
 
     # Удалить временные папки
     shutil.rmtree(os.path.join(project_root, "cobra"), ignore_errors=True)
