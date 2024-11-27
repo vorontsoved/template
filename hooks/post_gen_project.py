9e070e8c704585e10414b73277ca68e8636be855
e.vorontsov <e.vorontsov@eapteka.ru>
2024-11-27 15:35:28 +0300

    init

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,19 +5,13 @@ def setup_project():
     project_root = os.getcwd()
 
     if "{{ cookiecutter.include_cobra }}" == "yes":
+        # Переместить содержимое with_cobra в корень
         source_dir = os.path.join(project_root, "cobra")
     else:
+        # Переместить содержимое without_cobra в корень
         source_dir = os.path.join(project_root, "simple")
 
-    # Удалить содержимое корня проекта
-    for item in os.listdir(project_root):
-        item_path = os.path.join(project_root, item)
-        if os.path.isfile(item_path) or os.path.islink(item_path):
-            os.unlink(item_path)  # Удалить файл или символическую ссылку
-        elif os.path.isdir(item_path):
-            shutil.rmtree(item_path)  # Удалить директорию
-
-    # Переместить только содержимое из выбранного шаблона
+    # Переместить файлы из выбранного шаблона
     for item in os.listdir(source_dir):
         shutil.move(os.path.join(source_dir, item), project_root)
 
